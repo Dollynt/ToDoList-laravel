a8c5d345367a2a8c2f663cdd0b8f8e41ae13c8c0
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-06-21 12:25:42 -0300

    criação das migrations

---
+++ database/migrations/2023_06_21_151442_create_tarefas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            //nome com no máximo 50 cacteres e não nulo
+            $table->string('nome', 50)->nullable(false);
+            //descrição com no máximo 140 caracteres
+            $table->string('descricao', 140)->nullable();
+            //booleano se a tarefa foi finalizada ou não, não nulo
+            $table->boolean('finalizada')->nullable(false);
+            //data de término quando a tarefa for finalizada
+            $table->dateTime('data_termino')->nullable();
+            //prioridade da tarefa com definição dos valores, e valor padrão baixo caso em branco
+            $table->enum('prioridade', ['Baixa', 'Média', 'Alta'])->default('Baixa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
---
+++ database/migrations/2023_06_21_151524_create_membros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membros', function (Blueprint $table) {
+            $table->id();
+            //email unico e não nulo
+            $table->string('email')->nullable(false)->unique();
+            //nome não nulo
+            $table->string('nome')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membros');
+    }
+}
