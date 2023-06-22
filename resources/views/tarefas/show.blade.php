0f18a6d602c3447ec9c5f00aca8afdc3520bfe92
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-06-22 18:02:23 -0300

    reestruturação da view e estilização

--- resources/views/tarefas/show.blade.php
+++ resources/views/tarefas/show.blade.php
@@ -1,9 +1,60 @@
+<link rel="stylesheet" href="{{asset('css/tarefas/tarefas_show.css')}}">
 <head>
-    <link rel="stylesheet" href="{{asset('css/tarefas/tarefas_show.css')}}">
+    <title>Edição de Tarefa</title>
 </head>
-<div class="task">
-    <div class="task-name">{{ $tarefa->nome }}</div>
-    <div class="task-priority">Prioridade: {{ $tarefa->prioridade }}</div>
-    <div class="task-status">Finalizada: {{ $tarefa->finalizada ? 'Sim' : 'Não' }}</div>
-    <div class="task-description">Descrição: {{$tarefa->descricao}}</div>
-</div>
+<body>
+    <h1 id="cadastro">Edição de Tarefa</h1>
+
+    <form method="POST" action="/tarefas/{{$tarefa->id}}" id="form">
+        @csrf
+
+        <div>
+            <label for="nome">Nome da tarefa:</label>
+            <input type="text" class="@error('nome') is-invalid @enderror" name="nome" value="{{$tarefa->nome}}">
+            <!--mensagem de erro de validação-->
+            @error('nome')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <div>
+            <label for="descricao">Descrição:</label>
+            <textarea type="text" class="@error('descricao') is-invalid @enderror" name="descricao">{{$tarefa->descricao}}</textarea>
+            <!--mensagem de erro de validação-->
+            @error('descricao')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <div>
+            <label for="edit-status">Finalizada:</label>
+            <select id="finalizada" name="finalizada" required>
+                <option value="False" {{ $tarefa->finalizada ? '' : 'selected' }}>Não</option>
+                <option value="True" {{ $tarefa->finalizada ? 'selected' : '' }}>Sim</option>
+            </select>
+        </div>
+
+        <div>
+            <label for="data-termino">Data de Término:{{$tarefa->data_termino}}</label>
+        </div>
+
+        <div>
+            <label for="prioridade">Prioridade:</label>
+            <select id="prioridade" name="prioridade">
+                <option value="Baixa" {{ $tarefa->prioridade == 'Baixa' ? 'selected' : '' }}>Baixa</option>
+                <option value="Média" {{ $tarefa->prioridade == 'Média' ? 'selected' : '' }}>Média</option>
+                <option value="Alta" {{ $tarefa->prioridade == 'Alta' ? 'selected' : '' }}>Alta</option>
+            </select>
+        </div>
+
+        <button type="submit" id="button_submit">Salvar</button>
+        @method('DELETE')
+        <button type="submit" class="delete-button">Excluir</button>
+
+
+    </form>
+</body>
