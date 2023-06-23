4e3a802e23e8b3a055aca130b83ab7db8e404143
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-06-23 13:20:19 -0300

    criação da view

---
+++ resources/views/nav_bar.blade.php
@@ -0,0 +1,25 @@
+<head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/homepage/homepage.css') }}">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/homepage">Home</a></li>
+                <li><a href="/membros/create">Cadastro de Membros</a></li>
+                <li><a href="/tarefas">Lista de Tarefas</a></li>
+                <li><a href="/tarefas/create">Cadastro de Tarefas</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>&copy; 2023 LuizGuylherme</p>
+    </footer>
+</body>
+
