a3616d76671c888696fc6979fa7fc41d81974a0f
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-06-23 12:51:42 -0300

    autenticação das rotas

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,21 @@ use App\Http\Controllers\LoginController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/homepage', [HomePageController::class, 'index'])->name('homepage.index');
+
 Route::get('/login', [LoginController::class, 'index'])->name('login.index');
 Route::post('/login', [LoginController::class, 'login'])->name('login.login');
-
-Route::get('/membros', [MembroController::class, 'index'])->name('membros.index');
 Route::get('/membros/create', [MembroController::class, 'create'])->name('membros.create');
 Route::post('/membros', [MembroController::class, 'store'])->name('membros.store');
-Route::get('/tarefas', [TarefaController::class, 'index'])->name('tarefas.index');
-Route::get('/tarefas/create', [TarefaController::class, 'create'])->name('tarefas.create');
-Route::post('/tarefas', [TarefaController::class, 'store'])->name('tarefas.store');
-Route::get('/tarefas/{tarefaId}', [TarefaController::class, 'show'])->name('tarefas.show');
-Route::put('/tarefas/{tarefaId}', [TarefaController::class, 'edit'])->name('tarefas.edit');
-Route::put('/tarefas/{tarefaId}', [TarefaController::class, 'finalizada_update'])->name('tarefas.finalizada_update');
-Route::delete('/tarefas/{tarefaId}', [TarefaController::class, 'delete'])->name('tarefas.delete');
 
+Route::middleware(['custom.auth'])->group(function () {
+    Route::get('/homepage', [HomePageController::class, 'index'])->name('homepage.index');
+    Route::get('/membros', [MembroController::class, 'index'])->name('membros.index');
+    Route::get('/tarefas', [TarefaController::class, 'index'])->name('tarefas.index');
+    Route::get('/tarefas/create', [TarefaController::class, 'create'])->name('tarefas.create');
+    Route::post('/tarefas', [TarefaController::class, 'store'])->name('tarefas.store');
+    Route::get('/tarefas/{tarefaId}', [TarefaController::class, 'show'])->name('tarefas.show');
+    Route::put('/tarefas/{tarefaId}', [TarefaController::class, 'edit'])->name('tarefas.edit');
+    Route::put('/tarefas/{tarefaId}', [TarefaController::class, 'finalizada_update'])->name('tarefas.finalizada_update');
+    Route::delete('/tarefas/{tarefaId}', [TarefaController::class, 'delete'])->name('tarefas.delete');
+});
 
